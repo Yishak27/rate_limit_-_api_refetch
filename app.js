db18949953a35481f0b54caa23627bb4ce6fab6e
PC <yisumaid3@gmail.com>
2025-11-29 21:45:07 +0300

    - adding a middleware for incoming api log

--- app.js
+++ app.js
@@ -6,6 +6,12 @@ dotenv.config({
 const app = express();
 const port = process.env.PORT || 3091;
 
+app.use(express.json());
+app.use((req, res, next) => {
+    console.log('Incoming request on url: ' + req.url + " Method: " + req.method);
+    next();    
+})
+
 app.listen(port, () => {
     console.log("Backend api call service running on " + port);
 })
